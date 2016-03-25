924396c8b54cb12e1a869a66a8d137e55812d929
steve chesnowitz <steve@chesnowitz.com>
2016-03-25 14:12:52 +0000

    init commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+  
+  
+  def index
+    if current_user
+      redirect_to articles_path
+    end
+    @articles = Article.last(5)
+  end
+
+  def contact
+  @contact = Contact.new  
+  end
+
+  def about
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :articles do
+    resources :comments
+  end
+  resources :contacts 
+  root to: 'pages#index'
+  get 'pages/contact'
+  get 'pages/about'
+
+end
