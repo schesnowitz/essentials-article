7e5b432ae8e21fedc268bb867bfb8c05562dab86
steve chesnowitz <steve@chesnowitz.com>
2016-03-26 08:44:55 +0000

    validations and views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates_uniqueness_of :username       
+      
          
   has_many :articles
   has_many :comments 
